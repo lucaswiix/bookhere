0eee5dee14931e3869c85d1e00ee40eaafe66e8a
lucaswiix <lucasrocha.rosaborges@hotmail.com>
2019-12-10 03:21:16 -0300

    frontend typescript started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>running</p>
+       </header>
+    </div>
+  );
+}
+
+export default App;
